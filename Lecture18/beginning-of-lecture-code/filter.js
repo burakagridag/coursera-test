c1a9f6ea13351a51389e2dccce350f7ef43b2360
Burak Agridag <burakagridag@gmail.com>
2017-02-10 09:50:03 +0300

    first web file

--- Lecture18/beginning-of-lecture-code/filter.js
+++ Lecture18/beginning-of-lecture-code/filter.js
@@ -1,2 +1,15 @@
 var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log("Number array: ", numberArray);
+
+function above5filter(value) {
+  return value>5;
+}
+
+var filteredNumberArray = numberArray.filter(above5filter);
+
+console.log("filtered numbers: "+filteredNumberArray);
+
+var shoppingList = [
+  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+];
+console.log("Shopping List: ", shoppingList);
